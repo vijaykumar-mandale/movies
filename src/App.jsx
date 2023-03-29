74b26654694f5f4917e2ae6f7bde3cd4621a8ba9
Vijaykumar Mandale <vijaykumarmandale@gmail.com>
2023-03-29 10:03:23 +0530

    Redux toolkit setup with test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 import {fetchDataFromApi} from "./utils/api";
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice";
+
 function App() {
+  const dispatch = useDispatch();
+  const {url}=useSelector((state)=>state.home);
 
   useEffect(() => {
     apiTesting();
@@ -8,11 +13,14 @@ function App() {
   
   const apiTesting = () => {
     fetchDataFromApi('/movie/popular').then((res)=>{
-      console.log(res)
+      console.log(res);
+      dispatch(getApiConfiguration(res))
     })
   }
 
-  return <div className="App">App</div>;
+  return <div className="App">App
+  {url?.total_pages}
+  </div>;
 }
 
 export default App;
