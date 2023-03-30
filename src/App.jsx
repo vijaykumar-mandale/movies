cb01fef3b413d088eefa792b383c62a4e7eeb08a
Vijaykumar Mandale <vijaykumarmandale@gmail.com>
2023-03-30 19:00:39 +0530

    Homepage banner section design

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,28 @@ import PageNotFound from "./pages/404/pageNotFound";
 function App() {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
+  console.log(url);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      }
+      dispatch(getApiConfiguration(url));
     });
   };
 
   return (
     <BrowserRouter>
-    <Header />
+    {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/:mediaType/:id" element={<Details />}/>
@@ -37,7 +44,7 @@ function App() {
         <Route path="/explore/:mediaType" element={<Explore />}/>
         <Route path="*" element={<PageNotFound />}/>
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
