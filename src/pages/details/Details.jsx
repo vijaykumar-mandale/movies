3cebe23cdca70a77cb3d0a38306b4ad3ba14e1f5
Vijaykumar Mandale <vijaykumarmandale@gmail.com>
2023-04-01 16:06:47 +0530

    Movie details screen

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 import "./style.scss";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
 
 const Details = () => {
-  return <div></div>;
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+
+  return (
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+    </div>
+  );
 };
 
 export default Details;
